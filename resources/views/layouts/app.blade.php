3d9dba4c68b059f443759b3876383c65dc2ada49
WalterCorsini <corsini.walter@gmail.com>
2024-06-08 16:12:14 +0200

    project scaffolding add template

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
+  <title>Laravel-Boolando</title>
 
   <!-- direttive di vite in tutte le pagine che usano questo file -->
   @vite('resources/js/app.js')
