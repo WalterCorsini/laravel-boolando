4bb891419ce616e3b541fe56048dfae721620e04
WalterCorsini <corsini.walter@gmail.com>
2024-06-08 20:57:06 +0200

    fix footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,8 +1,8 @@
-<footer class="container-fluid">
-    <div class="container d-flex justify-content-around align-items-center text-white">
+<footer class="container-fluid d-flex justify-content-center align-items-center">
+    <div class="container d-flex justify-content-around text-white">
 
         {{-- left list --}}
-        <div class="d-flex flex-column">
+        <div class="list d-flex flex-column">
             <h3>Boolando s.r.l</h3>
             <ul class="d-flex gap-3">
                 <li>informazioni legali</li>
@@ -13,9 +13,9 @@
         {{-- /left list --}}
 
         {{-- right list --}}
-        <div class="d-flex flex-column justify-content-center align-items-center fw-bold fs-5">
+        <div class="list d-flex flex-column justify-content-center fw-bold fs-5">
             <span> Trovaci anche su</span>
-            <ul class="d-flex gap-2 justify-content-center align-items-center text-white">
+            <ul class="d-flex gap-2 justify-content-center text-white">
                 <li>O</li>
                 <li>O</li>
                 <li>O</li>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,14 +1,19 @@
 <header class="d-flex justify-content-between align-items-center ps-5 pe-5">
-    <ul class="d-flex justify-content-center align-items-center gap-3">
-        <li class="{{ Request::route()->getName() === 'home' ? 'active' : '' }}"><a href="{{ route('home') }}" class="text-white">Home</a></li>
-        <li class="{{ Request::route()->getName() === 'donna' ? 'active' : '' }}"><a href="{{ route('donna') }}" class="text-white">Donna</a></li>
-        <li class="{{ Request::route()->getName() === 'uomo' ? 'active' : '' }}"><a href="{{ route('uomo') }}" class="text-white">Uomo</a></li>
+    <nav>
+        <ul class="d-flex justify-content-center align-items-center gap-3">
+            <li class="{{ Request::route()->getName() === 'home' ? 'active' : '' }}"><a href="{{ route('home') }}"
+                    class="text-white">Home</a></li>
+            <li class="{{ Request::route()->getName() === 'donna' ? 'active' : '' }}"><a href="{{ route('donna') }}"
+                    class="text-white">Donna</a></li>
+            <li class="{{ Request::route()->getName() === 'uomo' ? 'active' : '' }}"><a href="{{ route('uomo') }}"
+                    class="text-white">Uomo</a></li>
         </ul>
-        {{-- logo --}}
-        <img src="{{ Vite::asset('resources/img/logo boolando.png') }}" alt="logo">
-        <div>
-            <span class="text-white"><a class="text-white" href=""> O </a></span>
-            <span class="text-white"><a class="text-white" href=""> O </a></span>
-            <span class="text-white"><a class="text-white" href=""> O </a></span>
-        </div>
+    </nav>
+    {{-- logo --}}
+    <img src="{{ Vite::asset('resources/img/logo boolando.png') }}" alt="logo">
+    <div>
+        <span class="text-white"><a class="text-white" href=""> O </a></span>
+        <span class="text-white"><a class="text-white" href=""> O </a></span>
+        <span class="text-white"><a class="text-white" href=""> O </a></span>
+    </div>
 </header>
